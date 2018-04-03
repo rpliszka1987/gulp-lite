38abe8256d1f1d1f2e27ddcb0f69fa764582986b
Rob Pliszka <robpliszka@gmail.com>
2018-04-02 20:48:19 -0400

    Css styling

---
+++ page-empty.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="/dev/wp-content/themes/gulp-lite/favicon.ico" type="image/x-icon" />
+    <link rel='stylesheet' href='/dev/wp-content/themes/gulp-lite/style.css' type='text/css' media='all' />
+    <title>Empty</title>
+</head>
+<body>
+    <header>
+        <h1> Hi </h1>
+    </header>
+    <div id="page" class="container">
+        Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.
+    </div>
+    <footer>
+        footer
+    </footer>
+</body>
+</html>
